f9204e2bb112eacfd530ab1955a23f962b0e979e
Ahmet Karakaya <karakayahmet00@gmail.com>
2022-10-13 10:00:06 +0300

    first commit

---
+++ nltk-tokenize.py
@@ -0,0 +1,18 @@
+from lib2to3.pgen2 import token
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+text = "Günlerdir gündem eski bir TSK mensubu, Mehmet Ali Çelebi. Tanınmış bir isim. Türk Silahlı Kuvvetleri’nde teğmenken Ergenekon soruşturması kapsamında tutuklandı."
+
+word_tokenler = word_tokenize(text)
+
+print(word_tokenler)
+
+sent_tokenler = sent_tokenize(text)
+
+print(sent_tokenler)
+
+for token in word_tokenize(text):
+    print(token)
+
+for token in sent_tokenize(text):
+    print(token)
